f884c1621ea523f1d18fdf305885b79fc27fd1c3
dlzagu <eodnsdlekd@naver.com>
2022-11-21 18:01:57 +0900

    feat : added style

--- front/src/components/global/Header/index.tsx
+++ front/src/components/global/Header/index.tsx
@@ -23,14 +23,16 @@ const HeaderContainer = styled.nav`
   width: 100%;
   position: fixed;
   top: 0;
+  padding: 0 8%;
   background-color: ${(props) => props.theme.themeColor};
+  background: none;
   z-index: 50;
 `;
 
 const ContentContainer = styled.div`
   width: 100%;
   height: 7rem;
-  padding: 0 4rem;
+
   ${(props) => props.theme.mixins.flexBox('row', 'center', 'space-between')}
 `;
 
