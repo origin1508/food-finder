21ca944c3c08e92813d48a18fb107bd0270f9988
origin1508 <origin1508@gmail.com>
2022-11-21 17:54:22 +0900

    Feat: AuthLoginForm

---
+++ front/src/components/auth/AuthLoginForm.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import {
+  AuthButton,
+  AuthButtonContainer,
+  AuthInput,
+  AuthInputContainer,
+} from '../../styles/authStyle';
+
+const AuthLoginForm = () => {
+  return (
+    <AuthLoginFormContainer>
+      <AuthInputContainer>
+        <AuthInput placeholder="E-mail" type="email" />
+      </AuthInputContainer>
+      <AuthInputContainer>
+        <AuthInput placeholder="Password" type="password" />
+      </AuthInputContainer>
+      <AuthButtonContainer>
+        <AuthButton>Login</AuthButton>
+        <AuthLink href="/register">Sign up</AuthLink>
+      </AuthButtonContainer>
+    </AuthLoginFormContainer>
+  );
+};
+
+export default AuthLoginForm;
+
+const AuthLoginFormContainer = styled.form`
+  ${(props) => props.theme.mixins.flexBox('column')}
+
+  width: 90%;
+  height: 100%;
+`;
+
+const AuthLink = styled.a`
+  width: 100%;
+  height: 5rem;
+  line-height: 5rem;
+  text-align: center;
+  color: #2e83f5;
+  cursor: pointer;
+`;
