94884ee38aa8caa1f90c9dc6257a10a3dec30f10
origin1508 <origin1508@gmail.com>
2022-11-22 14:55:06 +0900

    style: authStyle 수정

--- front/src/styles/authStyle.ts
+++ front/src/styles/authStyle.ts
@@ -22,10 +22,10 @@ export const AuthButton = styled.button`
   width: 100%;
   height: 5rem;
   font-size: 1.5rem;
-  margin-bottom: ${(props) => props.theme.spacingSemiMedium};
+  margin-bottom: ${({ theme }) => theme.spacingSemiMedium};
   border-radius: 0.5rem;
   background-color: #2e83f5;
-  color: ${(props) => props.theme.mainWhite};
+  color: ${({ theme }) => theme.mainWhite};
 
   &:active {
     transform: scale(0.98);
