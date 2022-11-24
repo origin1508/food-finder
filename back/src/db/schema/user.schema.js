735270cd8f4333ebb55f5571d8ed7af34367c59f
HyunSeong <hhs950120@gmail.com>
2022-11-24 11:05:42 +0900

    FEAT: 유저 스키마 생성

---
+++ back/src/db/schema/user.schema.js
@@ -0,0 +1,42 @@
+// import sequelize from
+// import { DataTypes } from 'sequelize';
+
+const User = sequelize.define(
+  "User",
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    profile_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue:
+        "https://m.nongmin.com/upload/news/202007/20200711032704602/20200711032704602.jpg",
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    tableName: "users",
+    charset: "utf8mb4",
+    collate: "utf8mb4_general_ci",
+  }
+);
+
+export default User;
