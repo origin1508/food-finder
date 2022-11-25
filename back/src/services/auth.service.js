384fddfd74db160037bad058e00b69be6ddea3a4
HyunSeong <hhs950120@gmail.com>
2022-11-25 18:10:29 +0900

    FEAT: 회원가입 서비스 추가

--- back/src/services/auth.service.js
+++ back/src/services/auth.service.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import ApiError from "../utils/ApiError";
 
 import userModel from "../db/model/user.model";
 
@@ -41,5 +42,23 @@ export default {
     const token = jwt.sign(payload, secretKey, options);
 
     return token;
+  },
+
+  async registerInfo(email, password, nickname) {
+    // 아래 로직 좀 더 다듬기
+    const exUserByEmail = await userModel.findByEmail(email);
+    console.log(exUserByEmail);
+    if (exUserByEmail) {
+      throw ApiError.setForbidden("이미 존재하는 이메일입니다.");
+    }
+
+    const exUserByNickname = await userModel.findByNickname(nickname);
+    if (exUserByNickname) {
+      throw ApiError.setForbidden("이미 존재하는 닉네임입니다.");
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 12);
+
+    await userModel.create(email, encryptedPassword, nickname);
   },
 };
