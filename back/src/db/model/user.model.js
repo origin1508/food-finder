9ee012e5ed85980230d1206e85250aa9d5dca144
HyunSeong <hhs950120@gmail.com>
2022-11-25 18:10:47 +0900

    FEAT: 회원가입 API model 추가 - findByNickname - create

--- back/src/db/model/user.model.js
+++ back/src/db/model/user.model.js
@@ -1,7 +1,7 @@
 import { User } from "../schema";
 
 export default {
-  async findByEmail(email) { 
+  async findByEmail(email) {
     const user = await User.findOne({
       attributes: ["user_id", "email", "nickname", "profile_url"],
       where: {
@@ -10,5 +10,24 @@ export default {
     });
 
     return user;
+  },
+
+  async findByNickname(nickname) {
+    const user = await User.findOne({
+      attributes: ["user_id", "email", "nickname", "profile_url"],
+      where: {
+        nickname,
+      },
+    });
+
+    return user;
+  },
+
+  async create(email, password, nickname) {
+    await User.create({
+      email,
+      password,
+      nickname,
+    });
   },
 };
