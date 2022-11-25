77ad201751cecbc11721d1f365398ae9dc51c9e3
EH <chi0727139@gmail.com>
2022-11-26 03:23:24 +0900

    FEAT : cors / axios 추가

--- back/src/app.js
+++ back/src/app.js
@@ -1,28 +1,25 @@
 import express from "express";
+import cors from "cors";
 import sequelize from "./configs/sequelize";
 import errorMiddleware from "./middlewares/error";
 
-
 // 라우터 모듈
-import authRouter from './routers/auth.route';
+import authRouter from "./routers/auth.route";
 
 const app = express();
-
-
 
 sequelize.sync({ sync: false });
 // sequelize.sync({ alter: true });
 
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("food-finder");
 });
-
-
 
 // multer 테스트 api
 // app.post("/", userProfileImageUpload.single("image"), (req, res) => {
