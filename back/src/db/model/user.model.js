c748f6ce02c228c37fa885eb471aa4fc64e340cc
HyunSeong <hhs950120@gmail.com>
2022-11-25 18:54:59 +0900

    FIX: findByEamil 모든 컬럼 리턴하도록 수정

--- back/src/db/model/user.model.js
+++ back/src/db/model/user.model.js
@@ -3,7 +3,6 @@ import { User } from "../schema";
 export default {
   async findByEmail(email) {
     const user = await User.findOne({
-      attributes: ["user_id", "email", "nickname", "profile_url"],
       where: {
         email,
       },
