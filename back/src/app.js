6f6532a9991be19918d89af60ad0954edf02a96e
HyunSeong <hhs950120@gmail.com>
2022-11-25 18:11:14 +0900

    FEAT: json 포맷 요청을 위해 미들웨어 추가, 회원가입 라우터 app 객체와 연결

--- back/src/app.js
+++ back/src/app.js
@@ -2,14 +2,27 @@ import express from "express";
 import sequelize from "./configs/sequelize";
 import errorMiddleware from "./middlewares/error";
 
+
+// 라우터 모듈
+import authRouter from './routers/auth.route';
+
 const app = express();
+
+
 
 sequelize.sync({ sync: false });
 // sequelize.sync({ alter: true });
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use("/auth", authRouter);
+
 app.get("/", (req, res) => {
   res.send("food-finder");
 });
+
+
 
 // multer 테스트 api
 // app.post("/", userProfileImageUpload.single("image"), (req, res) => {
