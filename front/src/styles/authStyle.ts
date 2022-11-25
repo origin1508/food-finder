627718d2e89e2aad42d2b5a7ee6fd55f4d3df56e
origin1508 <origin1508@gmail.com>
2022-11-25 17:03:40 +0900

    style: login button 스타일 수정

--- front/src/styles/authStyle.ts
+++ front/src/styles/authStyle.ts
@@ -13,18 +13,16 @@ export const AuthButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
   width: 100%;
   height: 15rem;
 `;
 
 export const AuthButton = styled.button`
+  ${({ theme }) => theme.mixins.mediumButton()};
   width: 100%;
   height: 5rem;
-  font-size: 1.5rem;
   margin-bottom: ${({ theme }) => theme.spacingSemiMedium};
-  border-radius: 0.5rem;
-  background-color: #2e83f5;
+  background-color: ${({ theme }) => theme.themeColor};
   color: ${({ theme }) => theme.mainWhite};
 
   &:active {
