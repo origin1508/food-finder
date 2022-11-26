7cfc4f04507b599bda9e979522adf78404868a97
HyunSeong <hhs950120@gmail.com>
2022-11-26 17:53:49 +0900

    FEAT: user 서비스 - modifyPassword 메소드 : 유저 비밀번호 수정 - checkCorrectPassword 메소드 : 기존 비밀번호 일치 확인

--- back/src/services/user.service.js
+++ back/src/services/user.service.js
@@ -1,9 +1,25 @@
-import userModel from '../db/model/user.model'
+import userModel from "../db/model/user.model";
+import bcrypt from "bcrypt";
+import ApiError from "../utils/ApiError";
 
 export default {
   async modifyNickname(userId, nickname) {
     const user = await userModel.updateNickname(userId, nickname);
-    
+
     return user;
-  }
-}
+  },
+
+  async modifyPassword(userId, password) {
+    const encryptedPassword = await bcrypt.hash(password, 12);
+    await userModel.updatePassword(userId, encryptedPassword);
+  },
+
+  async checkCorrectPassword(userId, password) {
+    const user = await userModel.findById(userId);
+
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+    if (!isCorrectPassword) {
+      throw ApiError.setBadRequest("일치하지 않는 비밀번호입니다.");
+    }
+  },
+};
