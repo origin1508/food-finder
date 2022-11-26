10dfa05b60b24830a53f0bd51e0b581cc74b11c3
HyunSeong <hhs950120@gmail.com>
2022-11-26 17:52:22 +0900

    FEAT: user API 라우터 - PUT /users/:userId/password -> 유저 비밀번호 수정

--- back/src/routers/user.route.js
+++ back/src/routers/user.route.js
@@ -21,4 +21,21 @@ router.put("/:userId/nickname", async (req, res, next) => {
   }
 });
 
+router.put("/:userId/password", async (req, res, next) => {
+  const { userId } = req.params;
+  const { password, newPassword } = req.body;
+
+  try {
+    await userService.checkCorrectPassword(userId, password);
+    await userService.modifyPassword(userId, newPassword);
+
+    res.status(201).json({
+      success: true,
+      message: "비밀번호 수정 성공",
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
 export default router;
