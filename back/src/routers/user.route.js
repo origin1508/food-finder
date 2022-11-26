e34e6637ef5f00bf0b2bebd89b7940aa6890789a
HyunSeong <hhs950120@gmail.com>
2022-11-26 19:07:50 +0900

    FEAT: 유저 프로필 변경 API - PUT /users/:userId/profileImage

--- back/src/routers/user.route.js
+++ back/src/routers/user.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 
 import userService from "../services/user.service";
+import { userProfileImageUpload } from "../middlewares/multer";
+import ApiError from '../utils/ApiError';
 
 const router = express.Router();
 
 router.put("/:userId/nickname", async (req, res, next) => {
   const { userId } = req.params;
-  const { nickname} = req.body;
+  const { nickname } = req.body;
 
   try {
     await userService.checkDuplicatedNickname(nickname);
@@ -37,5 +39,29 @@ router.put("/:userId/password", async (req, res, next) => {
     next(err);
   }
 });
+
+router.put(
+  "/:userId/profileImage",
+  userProfileImageUpload.single("profileImage"),
+  async (req, res, next) => {
+    if (!req.file) {
+      throw ApiError.setBadRequest('이미지 파일을 전송받지 못했습니다.');
+    }
+
+    const { userId } = req.params;
+    const { location } = req.file;
+
+    try {
+      await userService.modifyProfileImage(userId, location);
+
+      res.status(201).json({
+        success: true,
+        message: "유저 프로필 이미지 변경 성공",
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 export default router;
