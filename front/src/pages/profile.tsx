feb383a22b4e4184ae8c5a74ebc3fd4fc9b2571f
dlzagu <eodnsdlekd@naver.com>
2022-11-28 18:45:50 +0900

    style : added profile

---
+++ front/src/pages/profile.tsx
@@ -0,0 +1,37 @@
+import BasePageComponent from '../hoc/BasePageComponent';
+import styled from 'styled-components';
+import HeaderBgTheme from '../components/global/Header/HeaderBgTheme';
+import { MediumTitle } from '../styles/commonStyle';
+import CurrentUserProfile from '../components/profile/CurrentUserProfile';
+import UserRecipe from '../components/profile/UserRecipe';
+const Profile = () => {
+  return (
+    <>
+      <HeaderBgTheme></HeaderBgTheme>
+
+      <Container>
+        <ContentContainer>
+          <CurrentUserProfile></CurrentUserProfile>
+          <UserRecipe></UserRecipe>
+        </ContentContainer>
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.article`
+  width: 100%;
+  height: 100vh;
+  padding-top: 8vh;
+`;
+
+const ContentContainer = styled.div`
+  ${({ theme }) => theme.mixins.flexBox('row', 'center', 'space-around')}
+  padding: 0 8%;
+  width: 100%;
+  height: 100%;
+  gap: ${({ theme }) => theme.spancingLarge};
+  margin: ${({ theme }) => `${theme.spancingMedium} 0`};
+`;
+
+export default Profile;
