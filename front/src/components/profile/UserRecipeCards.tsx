167ba043334e6727d0d8b02bb8e3a552c3e7b75b
dlzagu <eodnsdlekd@naver.com>
2022-11-28 18:44:45 +0900

    style : added UserRecipeCadrs

---
+++ front/src/components/profile/UserRecipeCards.tsx
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import RecipeCard from '../recipe/RecipeCard';
+import { MediumTitle } from '../../styles/commonStyle';
+import CustomIcon from '../icons/CustomIcon';
+import { theme } from '../../styles/theme';
+import { BaseComponentType } from '../../types/common/baseComponentType';
+import mockData from '../../util/mockData';
+const UserRecipeCards = ({ children }: BaseComponentType) => {
+  const CARD_WIDTH_SIZE = 23.4;
+  const CARDS_WIDTH_SIZE = 132;
+  const { recipeDatas } = mockData;
+  const [slidePx, setSlidePx] = useState(0);
+
+  const toPrev = () => {
+    slidePx < 0 && setSlidePx(slidePx + CARD_WIDTH_SIZE);
+  };
+
+  const toNext = () => {
+    slidePx > -CARDS_WIDTH_SIZE && setSlidePx(slidePx - CARD_WIDTH_SIZE);
+  };
+
+  return (
+    <UserRecipeCardsContainer>
+      <Title>{children}</Title>
+      <RecipeCards>
+        <Wrap results={slidePx}>
+          {recipeDatas.map((recipe) => {
+            return (
+              <RecipeCard
+                img={recipe.img}
+                title={recipe.title}
+                channelUuid={recipe.channelUuid}
+                views={recipe.views}
+                likes={recipe.likes}
+                creator={recipe.creator}
+                onMoreClick={recipe.onMoreClick}
+                index={recipe.index}
+                size="20"
+              ></RecipeCard>
+            );
+          })}
+        </Wrap>
+      </RecipeCards>
+      <PrevButton onClick={() => toPrev()}>
+        <CustomIcon name="prev" size="2.5vh" color={theme.mainBlack} />
+      </PrevButton>
+      <NextButton onClick={() => toNext()}>
+        <CustomIcon name="next" size="2.5vh" color={theme.mainBlack} />
+      </NextButton>
+    </UserRecipeCardsContainer>
+  );
+};
+
+const UserRecipeCardsContainer = styled.div`
+  position: relative;
+  ${({ theme }) => theme.mixins.flexBox('column')};
+  gap: 4vh;
+`;
+
+const Title = styled.h2`
+  ${MediumTitle}
+  color:${({ theme }) => theme.mainBlack}
+`;
+
+const RecipeCards = styled.div`
+  max-width: 67vh;
+  height: 21vh;
+  overflow: hidden;
+`;
+const Wrap = styled.div`
+  ${({ theme }) => theme.mixins.flexBox('row', 'center', 'start')}
+  gap: 3rem;
+  transform: translateX(${({ results }) => `${results}vh`});
+  transition: all 0.3s;
+`;
+
+const PrevButton = styled.div`
+  ${({ theme }) => theme.mixins.flexBox}
+  position: absolute;
+  cursor: pointer;
+  top: 50%;
+  left: -2.5%;
+  background-color: ${({ theme }) => theme.mainWhite};
+  width: 4vh;
+  height: 4vh;
+  border-radius: 2rem;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
+
+const NextButton = styled.div`
+  ${({ theme }) => theme.mixins.flexBox}
+  position: absolute;
+  cursor: pointer;
+  top: 50%;
+  right: -2.5%;
+  background-color: ${({ theme }) => theme.mainWhite};
+  width: 4vh;
+  height: 4vh;
+  border-radius: 2rem;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
+
+export default UserRecipeCards;
