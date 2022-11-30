928b7e9fd529660366c9f8abb1e785e7bcfc7a74
jaehee <jaeheelee169@gmail.com>
2022-11-30 17:15:47 +0900

    feat: 레시피 정보 불러오기 시 likes 항목 추가

--- back/src/db/model/recipe.model.js
+++ back/src/db/model/recipe.model.js
@@ -1,4 +1,4 @@
-import { Recipe, RecipeLike, User } from "../schema";
+import { Recipe, RecipeLike, User, RecipeComment, RecipeStar } from "../schema";
 import { Op, Sequelize } from "sequelize";
 
 export default {
@@ -10,7 +10,19 @@ export default {
         ["image_url1", "smallThumbnailUrl"],
         ["image_url2", "largeThumbnailUrl"],
         "views",
+        [
+          Sequelize.fn("COUNT", Sequelize.col("`RecipeLikes`.`user_id`")),
+          "likes",
+        ],
       ],
+      include: [
+        {
+          model: RecipeLike,
+          attributes: [],
+          required: false,
+        },
+      ],
+      group: ["dish_id"],
     });
 
     return recipes;
