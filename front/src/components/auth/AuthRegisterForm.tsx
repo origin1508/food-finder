b4594d7f0558ac358fe343738edf6d141b36d258
dlzagu <eodnsdlekd@naver.com>
2022-12-01 21:02:14 +0900

    feat : api 연결

--- front/src/components/auth/AuthRegisterForm.tsx
+++ front/src/components/auth/AuthRegisterForm.tsx
@@ -1,3 +1,6 @@
+import { useForm } from 'react-hook-form';
+import { AuthFormInitial } from '../../types/auth';
+import useSignup from '../../hooks/useSignup';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import {
@@ -8,22 +11,91 @@ import {
 } from '../../styles/authStyle';
 
 const AuthRegisterForm = () => {
+  const { mutate: signup, isLoading: signupLoading } = useSignup();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm<AuthFormInitial>({
+    mode: 'onChange',
+    defaultValues: {
+      email: '',
+      nickname: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const onSubmitRegister = handleSubmit((registerForm) => {
+    delete registerForm.confirmPassword;
+    signup(registerForm);
+  });
+
   return (
-    <AuthRegisterFormContainer>
+    <AuthRegisterFormContainer onSubmit={onSubmitRegister}>
       <AuthInputContainer>
-        <AuthInput placeholder="Your Name" />
+        <AuthInput
+          placeholder="Your Name"
+          type="name"
+          {...register('nickname', {
+            required: '닉네임을 입력해주세요',
+            minLength: {
+              value: 3,
+              message: '닉네임은 3글자 이상 입력해주세요.',
+            },
+          })}
+        />
+        <ErrorMessage>{errors.nickname?.message}</ErrorMessage>
       </AuthInputContainer>
       <AuthInputContainer>
-        <AuthInput placeholder="E-mail" type="email" />
+        <AuthInput
+          placeholder="E-mail"
+          type="email"
+          {...register('email', {
+            required: '이메일을 입력해주세요.',
+            pattern: {
+              value:
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: '이메일 형식에 맞지 않습니다!',
+            },
+          })}
+        />
+        <ErrorMessage>{errors.email?.message}</ErrorMessage>
       </AuthInputContainer>
       <AuthInputContainer>
-        <AuthInput placeholder="Password" type="password" />
+        <AuthInput
+          placeholder="Password"
+          type="password"
+          {...register('password', {
+            required: { value: true, message: '비밀번호를 입력해주세요.' },
+            pattern: {
+              value: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/,
+              message: '숫자,특수문자,영문 포함 8자리 이상 적어주세요.',
+            },
+          })}
+        />
+        <ErrorMessage>{errors.password?.message}</ErrorMessage>
       </AuthInputContainer>
       <AuthInputContainer>
-        <AuthInput placeholder="Confirm Password" type="password" />
+        <AuthInput
+          placeholder="Confirm Password"
+          type="password"
+          {...register('confirmPassword', {
+            required: '비밀번호를 한번 더 입력해 주세요',
+            validate: {
+              mathchesPreviousPassword: (value) => {
+                const { password } = getValues();
+                return password === value || '비밀번호가 일치하지 않습니다.';
+              },
+            },
+          })}
+        />
+        <ErrorMessage>{errors.confirmPassword?.message}</ErrorMessage>
       </AuthInputContainer>
       <AuthButtonContainer>
-        <AuthButton>Registration</AuthButton>
+        <AuthButton type="submit">Registration</AuthButton>
         <AuthLink to="/login">Sign in</AuthLink>
       </AuthButtonContainer>
     </AuthRegisterFormContainer>
@@ -46,4 +118,12 @@ const AuthLink = styled(Link)`
   text-align: center;
   color: #2e83f5;
   cursor: pointer;
+`;
+const ErrorMessage = styled.div`
+  position: absolute;
+  font-size: ${({ theme }) => theme.fontSmall};
+  color: ${({ theme }) => theme.lightRed};
+  height: 1.4rem;
+  left: 2rem;
+  bottom: -1.5rem;
 `;
