5cfd6ecfe1ccf89f8bfe1d364ecc163cf2859b0e
origin1508 <origin1508@gmail.com>
2022-12-01 10:09:36 +0900

    fix: naver api 관련 삭제

--- front/src/components/searchResult/PlaceResult.tsx
+++ front/src/components/searchResult/PlaceResult.tsx
@@ -1,90 +1,13 @@
-import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import Search from '../common/Search';
+import KakaoMap from '../map/KakaoMap';
 import { MediumTitle } from '../../styles/commonStyle';
-import axios from 'axios';
-
-const { VITE_NAVER_DEV_CLIENT_ID, VITE_NAVER_DEV_CLIENT_SECRET } = import.meta
-  .env;
-
-interface Data {
-  local: string;
-  food: string;
-}
-
-interface PlaceList {
-  address: string;
-  category: string;
-  description: string;
-  link: string;
-  mapx: string;
-  mapy: string;
-  roadAddress: string;
-  telephone: string;
-  title: string;
-  thumbnail?: string;
-}
 
 const PlaceResult = () => {
-  const MapRef = useRef<HTMLDivElement>(null);
-  const [placeList, setPlaceList] = useState<PlaceList[]>();
-
-  const getSearchImg = async (address: string) => {
-    const endpoint = '/naverApi/v1/search/image';
-    const display = 5;
-    const res = await axios.get(endpoint, {
-      params: {
-        query: address,
-        display: display,
-      },
-      headers: {
-        'X-Naver-Client-Id': VITE_NAVER_DEV_CLIENT_ID,
-        'X-Naver-Client-Secret': VITE_NAVER_DEV_CLIENT_SECRET,
-      },
-    });
-    console.log('이미지검색 :', res.data.items);
-    const { thumbnail } = res.data.items[0];
-    return thumbnail;
-  };
-
-  const handleSearchClick = async () => {
-    const endpoint = '/naverApi/v1/search/local.json';
-    const query = '김치찌개 잠실역';
-
-    const res = await axios.get(endpoint, {
-      params: {
-        query: query,
-        display: 5,
-      },
-      headers: {
-        'X-Naver-Client-Id': VITE_NAVER_DEV_CLIENT_ID,
-        'X-Naver-Client-Secret': VITE_NAVER_DEV_CLIENT_SECRET,
-      },
-    });
-    const items: [PlaceList] = res.data.items;
-    // items.map((item) => {
-    //   const { title, roadAddress } = item;
-    //   const thumbnail = getSearchImg(title + ' ' + roadAddress);
-    //   return { ...item, thumbnail: thumbnail };
-    // });
-
-    console.log(items);
-    getSearchImg('서울김밥');
-  };
-  useEffect(() => {
-    const map = MapRef.current && new window.naver.maps.Map(MapRef.current);
-  }, []);
   return (
     <PlaceResultContainer>
       <RecipeResultTitle>"김치찌개" 맛집</RecipeResultTitle>
       <MapContainer>
-        <PlaceListContainer>
-          <Search
-            onSearchClick={handleSearchClick}
-            placeholder="지역을 입력해주세요"
-          />
-        </PlaceListContainer>
-        <Map ref={MapRef}></Map>
+        <KakaoMap />
       </MapContainer>
     </PlaceResultContainer>
   );
@@ -114,16 +37,4 @@ const MapContainer = styled.section`
   border-radius: 0.5rem;
   overflow: hidden;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-`;
-
-const PlaceListContainer = styled.section`
-  ${({ theme }) => theme.mixins.flexBox('column', 'center', 'start')};
-  justify-content: 
-  width: 30%;
-  height: 100%;
-`;
-
-const Map = styled.div`
-  width: 80%;
-  height: 100%;
 `;
