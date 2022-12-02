0256af0f77cc41b80b9cbe902abbb27ebbcf3c00
EH <chi0727139@gmail.com>
2022-12-02 10:54:28 +0900

    FIX : APIError 추가

--- back/src/services/recipeSearch.service.js
+++ back/src/services/recipeSearch.service.js
@@ -10,12 +10,18 @@ export default {
 
   async recipeRankingOn7days() {
     const recipeRanking = await RecipeModel.rankingOn7days();
+    if (!recipeRanking) {
+      throw ApiError.setInternalServerError("이번 주 레시피 불러오기 실패");
+    }
 
     return recipeRanking;
   },
 
   async getRandomRecipe() {
     const randomRecipe = await RecipeModel.getRandomRecipe();
+    if (!randomRecipe) {
+      throw ApiError.setInternalServerError("랜덤 레시피 불러오기 실패");
+    }
 
     return randomRecipe;
   },
