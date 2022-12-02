c9c9f464a93675c29fcd96c81d38a427d0c14fea
dlzagu <eodnsdlekd@naver.com>
2022-12-02 16:51:46 +0900

    feat : added useLogin hook

---
+++ front/src/hooks/useLogin.ts
@@ -0,0 +1,27 @@
+import { useMutation } from 'react-query';
+import { useSetRecoilState } from 'recoil';
+import { useNavigate } from 'react-router-dom';
+import { authLoginRequest } from '../api/authFetcher';
+import { PATH } from '../customRouter';
+import useSetAlert from './useSetAlert';
+import { authState } from '../atom/auth';
+
+export default function useLogin() {
+  const { setAlertSuccess, setAlertError } = useSetAlert();
+  const setAuthState = useSetRecoilState(authState);
+  const navigate = useNavigate();
+
+  const mutation = useMutation(authLoginRequest, {
+    onSuccess: (data) => {
+      setAuthState(data?.result);
+      setAlertSuccess({ success: data?.message });
+      navigate(PATH.MAIN);
+    },
+    onError: (error: any) => {
+      setAlertError({ error: error.message });
+      console.log(error.message);
+    },
+  });
+
+  return mutation;
+}
