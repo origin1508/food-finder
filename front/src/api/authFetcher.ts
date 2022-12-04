728e7fb84902df8e0b709680c692efd23117e6a1
dlzagu <eodnsdlekd@naver.com>
2022-12-04 23:56:59 +0900

    feat : added authFetcher

--- front/src/api/authFetcher.ts
+++ front/src/api/authFetcher.ts
@@ -25,16 +25,34 @@ export async function authLoginRequest(loginForm: AuthFormInitial) {
   return res.data;
 }
 
-export async function authProfileImageUpdate({
-  endPoint,
-  formData,
-}: EditImageForm) {
+export async function authProfileImageUpdate(
+  endPoint: string,
+  image: Blob,
+): Promise<string> {
+  const formData = new FormData();
+  formData.append('image', image);
   const res = await customAxios.put(endPoint, formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
       Authorization: `Bearer ${Storage.getToken()}`,
     },
   });
-  const { result } = res.data;
-  return result;
+
+  return res.data;
+}
+
+export async function authProfileNickUpdate({
+  endPoint,
+  nickname,
+}: {
+  endPoint: string;
+  nickname: string;
+}) {
+  const res = await customAxios.put(endPoint, nickname, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${Storage.getToken()}`,
+    },
+  });
+  return res.data;
 }
