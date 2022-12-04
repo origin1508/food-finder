2f87bd44e95988a95dfa0191812dafe2fabf34c1
dlzagu <eodnsdlekd@naver.com>
2022-12-05 03:00:21 +0900

    feat : added navigate

--- front/src/hooks/useEditPassword.ts
+++ front/src/hooks/useEditPassword.ts
@@ -1,13 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import { useMutation } from 'react-query';
 import { authPasswordUpdate } from '../api/authFetcher';
 import useSetAlert from './useSetAlert';
+import { PATH } from '../customRouter';
 
 export default function useEditPassword() {
   const { setAlertSuccess, setAlertError } = useSetAlert();
+  const navigate = useNavigate();
 
   const mutation = useMutation(authPasswordUpdate, {
     onSuccess: (data) => {
       setAlertSuccess({ success: data?.message });
+      navigate(PATH.LOGIN);
     },
     onError: (error: any) => {
       setAlertError({ error: error.response.data.message });
