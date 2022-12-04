154a2c2d965a7c832e1085797988da82cd655fac
dlzagu <eodnsdlekd@naver.com>
2022-12-04 23:58:07 +0900

    feat : added interceptor error handling

--- front/src/util/customAxios.ts
+++ front/src/util/customAxios.ts
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { AxiosError } from 'axios';
+import Storage from '../storage/storage';
+import CookieStorage from '../storage/cookie';
 
 const { VITE_SERVER_URL } = import.meta.env;
 
@@ -25,6 +28,28 @@ customAxios.interceptors.response.use(
   },
   (error) => {
     return Promise.reject(error);
+  },
+);
+
+customAxios.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (error) => {
+    if (error.response.status >= 400 && error.response.status < 500) {
+      const { statusText, data } = error.response;
+      if (
+        statusText === 'Unauthorized' &&
+        data.message === '유효기간이 만료된 토큰입니다.'
+      ) {
+        Storage.clearToken();
+        CookieStorage.clearToken();
+        window.location.replace('/login');
+      }
+      return Promise.reject(error);
+    } else if (error.response.status >= 500) {
+      return AxiosError;
+    }
   },
 );
 
