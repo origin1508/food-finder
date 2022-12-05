2762270ebef153eb6abbb7618166e3d10d9555f0
origin1508 <origin1508@gmail.com>
2022-12-06 02:04:22 +0900

    fix: rename main to info

---
+++ front/src/components/createRecipe/main/CreateRecipeInfoLeft.tsx
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+import { useFormContext } from 'react-hook-form';
+import { CreateRecipeInputStyle } from '../../../styles/createRecipeStyle';
+import { categoryOptions, cookingMethodOptions } from '../createRecipeOptions';
+
+const CreateRecipeInfoLeft = () => {
+  const { register } = useFormContext();
+  return (
+    <CreateRecipeInfoLeftContainer>
+      <CreateRecipeInfoInputContainer>
+        <CreateRecipeInfoInputLabel>레시피 제목</CreateRecipeInfoInputLabel>
+        <CreateRecipeInfoInput
+          {...register('name', { required: true })}
+          placeholder="예) 소고기 미역국"
+        />
+      </CreateRecipeInfoInputContainer>
+      <CreateRecipeInfoInputContainer>
+        <CreateRecipeInfoInputLabel>요리 정보</CreateRecipeInfoInputLabel>
+        <CreateRecipeSelect {...register('category', { required: true })}>
+          <CreateRecipeOption disabled>인원</CreateRecipeOption>
+        </CreateRecipeSelect>
+        <CreateRecipeInfoInput
+          {...register('cookingTime', { required: true })}
+        />
+      </CreateRecipeInfoInputContainer>
+      <CreateRecipeInfoSelectContainer>
+        <CreateRecipeInfoInputLabel>카테고리</CreateRecipeInfoInputLabel>
+        <CreateRecipeSelect {...register('category', { required: true })}>
+          <CreateRecipeOption value="" hidden>
+            분류별
+          </CreateRecipeOption>
+          {categoryOptions.map((category) => (
+            <CreateRecipeOption key={category}>{category}</CreateRecipeOption>
+          ))}
+          {/* <option disabled>종류별</option>
+          <option>밥</option>
+          <option>반찬</option>
+          <option>후식</option>
+          <option>국 & 찌개</option> */}
+        </CreateRecipeSelect>
+        <CreateRecipeSelect {...register('cookingMethod', { required: true })}>
+          <CreateRecipeOption value="" hidden>
+            조리방법별
+          </CreateRecipeOption>
+          {cookingMethodOptions.map((method) => (
+            <CreateRecipeOption key={method}>{method}</CreateRecipeOption>
+          ))}
+        </CreateRecipeSelect>
+      </CreateRecipeInfoSelectContainer>
+    </CreateRecipeInfoLeftContainer>
+  );
+};
+
+export default CreateRecipeInfoLeft;
+
+const CreateRecipeInfoLeftContainer = styled.section`
+  width: 60%;
+`;
+
+const CreateRecipeInfoInputContainer = styled.div`
+  ${({ theme }) => theme.mixins.flexBox()}
+  width: 100%;
+  margin-bottom: ${({ theme }) => theme.spacingMedium};
+`;
+
+const CreateRecipeInfoInput = styled.input`
+  ${CreateRecipeInputStyle}
+  margin: 0 ${({ theme }) => theme.spacingRegular};
+`;
+
+const CreateRecipeInfoInputLabel = styled.span`
+  flex-shrink: 0;
+  width: 16rem;
+  font-size: 2.25rem;
+`;
+
+const CreateRecipeInfoSelectContainer = styled.div`
+  ${({ theme }) => theme.mixins.flexBox()}
+`;
+
+const CreateRecipeSelect = styled.select`
+  ${CreateRecipeInputStyle}
+  margin: 0 ${({ theme }) => theme.spacingRegular};
+`;
+
+const CreateRecipeOption = styled.option``;
---
+++ front/src/components/createRecipe/main/CreateRecipeInfoRight.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { useFormContext } from 'react-hook-form';
+import CustomIcon from '../../icons/CustomIcon';
+import {
+  CreateRecipeImageInput,
+  CreateRecipeImageUploadStyle,
+} from '../../../styles/createRecipeStyle';
+
+const CreateRecipeInfoRight = () => {
+  const [previewUrl, setPreviewUrl] = useState('');
+  const { register } = useFormContext();
+  return (
+    <CreateRecipeInfoRightContainer>
+      <CreateRecipeInfoImageUpload previewUrl={previewUrl}>
+        <CreateRecipeImageInput
+          {...register('mainImage', { required: true })}
+          type="file"
+          accept="image/*"
+          onChange={(e) => {
+            if (e.target.files instanceof FileList) {
+              const uploadImage = e.target.files[0];
+              const previewUrl = URL.createObjectURL(uploadImage);
+              console.log(previewUrl);
+              setPreviewUrl(previewUrl);
+            }
+          }}
+        />
+        <ImageUploadIcon>
+          <CustomIcon name="imageUpload" size="40" color="grey" />
+          요리 대표 사진 등록
+        </ImageUploadIcon>
+      </CreateRecipeInfoImageUpload>
+    </CreateRecipeInfoRightContainer>
+  );
+};
+
+export default CreateRecipeInfoRight;
+
+const CreateRecipeInfoRightContainer = styled.section``;
+
+const CreateRecipeInfoImageUpload = styled.div<{
+  previewUrl: string | undefined;
+}>`
+  ${CreateRecipeImageUploadStyle}
+  ${({ previewUrl }) =>
+    previewUrl &&
+    `background-image: url(${previewUrl}); background-size: cover;`}
+`;
+
+const ImageUploadIcon = styled.div`
+  ${({ theme }) => theme.mixins.flexBox('column')}
+  width: 100%;
+  height: 100%;
+  gap: ${({ theme }) => theme.spacingRegular};
+  color: ${({ theme }) => theme.darkGrey};
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
