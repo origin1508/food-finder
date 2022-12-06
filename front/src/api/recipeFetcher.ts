a8668b6cc46387051325fa3114921175adde95f3
origin1508 <origin1508@gmail.com>
2022-12-06 23:23:54 +0900

    feat: recipeFetcher 생성

--- front/src/api/recipeFetcher.ts
+++ front/src/api/recipeFetcher.ts
@@ -3,32 +3,7 @@ import Storage from '../storage/storage';
 
 const JWT_TOKEN = Storage.getToken();
 
-export const createRecipeRequest = async (recipeForm) => {
-  const formData = new FormData();
-  const {
-    name,
-    method,
-    category,
-    ingredient,
-    serving,
-    cookingTime,
-    recipeThumnail,
-    stepImages,
-    steps,
-  } = recipeForm;
-
-  formData.append('name', name);
-  formData.append('method', method);
-  formData.append('category', category);
-  formData.append('ingredient', ingredient);
-  formData.append('serving', serving);
-  formData.append('cookingTime', cookingTime);
-  formData.append('recipeThumnail', recipeThumnail);
-  stepImages.forEach((stepImage) => {
-    formData.append('stepImages', stepImage);
-  });
-  formData.append('steps', steps);
-
+export const createRecipeRequest = async (formData: FormData) => {
   const res = await customAxios.post('/test/recipes', formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
