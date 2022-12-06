0b0e96a88583afb88ef191c4f82572ab91adaa00
origin1508 <origin1508@gmail.com>
2022-12-06 15:10:51 +0900

    fix: handleCreateRecipe 추가

--- front/src/components/createRecipe/CreateRecipeForm.tsx
+++ front/src/components/createRecipe/CreateRecipeForm.tsx
@@ -1,16 +1,54 @@
 import styled from 'styled-components';
 import { useFormContext } from 'react-hook-form';
-import CreateRecipeInfo from './main/CreateRecipeInfo';
+import CreateRecipeInfo from './info/CreateRecipeInfo';
 import CreateRecipeIngredient from './ingredient/CreateRecipeIngredient';
 import CreateRecipeInstruction from './instruction/CreateRecipeInstruction';
 import { CreateRecipeContainerStyle } from '../../styles/createRecipeStyle';
+import { createRecipeRequest } from '../../api/recipeFetcher';
+import imageResize from '../../util/imageResize';
+import { CreateRecipeValue } from '../../types/recipe/createRecipeType';
 
 const CreateRecipeForm = () => {
-  const { handleSubmit } = useFormContext();
+  const { handleSubmit } = useFormContext<CreateRecipeValue>();
+  const handleCreateRecipe = async (data: CreateRecipeValue) => {
+    const {
+      name,
+      mainImage,
+      serving,
+      cookingTime,
+      category,
+      method,
+      ingredients,
+      instructions,
+    } = data;
+    console.log(data);
+
+    const ingredient = 'test';
+
+    const stepImages = Array<Blob>();
+
+    const recipeThumbnail = await imageResize(mainImage[0]);
+    const steps = instructions.reduce(async (acc, cur, idx) => {
+      const { description, image } = cur;
+      const compressedImage = await imageResize(image[0]);
+      compressedImage && stepImages.push(compressedImage);
+      return { ...acc, [idx + 1]: description };
+    }, {});
+
+    const res = await createRecipeRequest({
+      name,
+      method,
+      category,
+      ingredient,
+      serving,
+      cookingTime,
+      recipeThumbnail,
+      stepImages,
+      steps,
+    });
+  };
   return (
-    <CreateRecipeFormConatiner
-      onSubmit={handleSubmit((data) => console.log(data))}
-    >
+    <CreateRecipeFormConatiner onSubmit={handleSubmit(handleCreateRecipe)}>
       <CreateRecipeInfo />
       <CreateRecipeIngredient />
       <CreateRecipeInstruction />
