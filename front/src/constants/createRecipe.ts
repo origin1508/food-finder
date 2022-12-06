1a8aa3158fe860a8b9e9f83d7a9e04a8eabdfe7c
origin1508 <origin1508@gmail.com>
2022-12-06 23:46:24 +0900

    feat: createRecipe 생성

---
+++ front/src/constants/createRecipe.ts
@@ -0,0 +1,36 @@
+export const IMAGE_FORMAT = ['image/jpeg', 'image/png'];
+
+export const INGREDIENT_PLACEHOLDERS = [
+  {
+    name: '예) 돼지고기',
+    amount: '예) 300g',
+  },
+  {
+    name: '예) 양배추',
+    amount: '예) 1/2개',
+  },
+  {
+    name: '예) 참기름',
+    amount: '예) 2t',
+  },
+];
+
+export const CATEGORY_OPTIONS = ['밥', '반찬', '후식', '국&찌개'];
+
+export const METHOD_OPTIONS = [
+  '볶기',
+  '찌기',
+  '튀기기',
+  '끓이기',
+  '굽기',
+  '기타',
+];
+
+export const SERVING_OPTIONS = [
+  '1인분',
+  '2인분',
+  '3인분',
+  '4인분',
+  '5인분',
+  '6인분이상',
+];
