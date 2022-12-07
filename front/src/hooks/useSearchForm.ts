8c85e6e3fd0f9f02246aec3c8665e7128086613b
origin1508 <origin1508@gmail.com>
2022-12-07 13:17:37 +0900

    fix: handleRecipeSearch 추가

--- front/src/hooks/useSearchForm.ts
+++ front/src/hooks/useSearchForm.ts
@@ -1,15 +1,30 @@
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import useSetAlert from './useSetAlert';
+import { searchKeywordRequest } from '../api/recipeFetcher';
 import { SearchValue } from '../types/search/searchType';
 
 const useSearchForm = () => {
-  const { register, handleSubmit } = useForm<SearchValue>({
+  const navigate = useNavigate();
+  const { setAlertError } = useSetAlert();
+  const { register, reset, handleSubmit } = useForm<SearchValue>({
     mode: 'onChange',
     defaultValues: {
       keyword: '',
     },
   });
+  const handleRecipeSearch = handleSubmit(async (data) => {
+    const { keyword } = data;
+    const res = await searchKeywordRequest(keyword);
+    console.log(res);
+    if (!res.success || res.result.length === 0) {
+      setAlertError({ error: '검색 결과가 존재하지 않습니다.' });
+    } else {
+      navigate(`/search/${keyword}`);
+    }
+  });
 
-  return { register, handleSubmit };
+  return { register, reset, handleSubmit, handleRecipeSearch };
 };
 
 export default useSearchForm;
