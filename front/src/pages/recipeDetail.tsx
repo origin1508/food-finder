06502c3b4950dcc10c928683105b642f33533a0a
dlzagu <eodnsdlekd@naver.com>
2022-12-07 18:54:38 +0900

    feat : import useRecipeDetail

--- front/src/pages/recipeDetail.tsx
+++ front/src/pages/recipeDetail.tsx
@@ -1,3 +1,6 @@
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useRecipeDetail } from '../hooks/Recipe/useRecipes';
 import styled from 'styled-components';
 import BasePageComponent from '../hoc/BasePageComponent';
 import RecipeDetailMain from '../components/recipeDetail/RecipeDetailMain';
@@ -6,10 +9,16 @@ import RecipeSteps from '../components/recipeDetail/RecipeSteps';
 import mockData from '../util/mockData';
 
 const RecipeDetail = () => {
+  const { userId } = useParams();
+  // const { data } = useRecipeDetail(userId!);
   const { recipeDetail } = mockData;
   const firstRecipeDetail = recipeDetail[0];
   const { ingredient } = recipeDetail[0];
   const { steps } = recipeDetail[0];
+
+  // useEffect(() => {
+  //   console.log(data);
+  // }, []);
   return (
     <BasePageComponent>
       <RecipeDetailContainer>
