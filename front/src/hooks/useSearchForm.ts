b4270cef59a75f2ae5d09f43788d0b0f554f1452
origin1508 <origin1508@gmail.com>
2022-12-07 16:42:22 +0900

    fix: recipeSearch 추가

--- front/src/hooks/useSearchForm.ts
+++ front/src/hooks/useSearchForm.ts
@@ -16,6 +16,7 @@ const useSearchForm = () => {
       keyword: '',
     },
   });
+
   const handleRecipeSearch = handleSubmit(async (data) => {
     const { keyword } = data;
     const res = await searchKeywordRequest(keyword);
@@ -29,7 +30,23 @@ const useSearchForm = () => {
     }
   });
 
-  return { register, reset, handleSubmit, handleRecipeSearch };
+  const recipeSearch = async (keyword: string) => {
+    const res = await searchKeywordRequest(keyword);
+    if (!res.success || res.result.length === 0) {
+      setAlertError({ error: '검색 결과가 존재하지 않습니다.' });
+    } else {
+      const { result, message } = res;
+      setAlertSuccess({ success: message });
+      setSearchResult(result);
+    }
+  };
+  return {
+    register,
+    reset,
+    handleSubmit,
+    handleRecipeSearch,
+    recipeSearch,
+  };
 };
 
 export default useSearchForm;
