36e8ccd266675e042a85fe43e9f4e9ba4cfd1f05
origin1508 <origin1508@gmail.com>
2022-12-08 03:38:00 +0900

    fix: getLikedRestaurant 추가

--- front/src/api/restaurantFetcher.ts
+++ front/src/api/restaurantFetcher.ts
@@ -4,6 +4,15 @@ import { Restaurant } from '../types/restaurant/restaurantType';
 
 const JWT_TOKEN = Storage.getToken();
 
+export const getLikedRestaurant = async () => {
+  const res = await customAxios.get(`/user/like/restaurant`, {
+    headers: {
+      Authorization: `Bearer ${JWT_TOKEN}`,
+    },
+  });
+  return res.data;
+};
+
 export const restaurantLikeRequest = async (restaurant: Restaurant) => {
   const res = await customAxios.post('/restaurant/like', restaurant, {
     headers: {
