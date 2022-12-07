cde43d51e4d31d55f9290bbc4ad70d4bec722969
origin1508 <origin1508@gmail.com>
2022-12-07 13:15:34 +0900

    fix: keyword 검색 기능 추가

--- front/src/components/global/Header/index.tsx
+++ front/src/components/global/Header/index.tsx
@@ -11,7 +11,7 @@ const Header = () => {
   const { pathname } = useLocation();
   const [scrollPosition, setScrollPosition] = useState(0);
   const [isBackground, setIsBackground] = useState(false);
-  const { register, handleSubmit } = useSearchForm();
+  const { register, handleRecipeSearch } = useSearchForm();
   const MIN_SCROLL_Y = 20;
 
   const updateScroll = () => {
@@ -48,7 +48,7 @@ const Header = () => {
             isBackground || scrollPosition > MIN_SCROLL_Y ? 'block' : 'none'
           }
           register={register}
-          onSubmit={handleSubmit(() => {})}
+          onSubmit={handleRecipeSearch}
         />
         <NavLink />
       </ContentContainer>
