9f67e2bd720711839e76c256b5599541a2f830c2
origin1508 <origin1508@gmail.com>
2022-12-07 13:20:42 +0900

    fix: PATH.SEARCH_RESULT 변경

--- front/src/customRouter/index.tsx
+++ front/src/customRouter/index.tsx
@@ -25,7 +25,7 @@ export const PATH = {
   RECIPE: '/recipe',
   CREATE_RECIPE: '/recipe/create',
   COLLECT_RECIPES: '/collectRecipes',
-  SEARCH_RESULT: '/search',
+  SEARCH_RESULT: '/search/:keyword',
   PROFILE: '/profile/:userId',
   RECIPE_DETAIL: '/recipe/detail',
   MAP: '/map',
