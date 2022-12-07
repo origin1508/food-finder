a6fc30b9d1d439e4c14a4e1e8ad78454696b1f5c
origin1508 <origin1508@gmail.com>
2022-12-07 16:08:06 +0900

    fix: keword를 query string으로 전달

--- front/src/hooks/useSearchForm.ts
+++ front/src/hooks/useSearchForm.ts
@@ -1,12 +1,15 @@
 import { useForm } from 'react-hook-form';
+import { useRecoilState } from 'recoil';
 import { useNavigate } from 'react-router-dom';
 import useSetAlert from './useSetAlert';
+import { searchResultState } from '../atom/searchResult';
 import { searchKeywordRequest } from '../api/recipeFetcher';
 import { SearchValue } from '../types/search/searchType';
 
 const useSearchForm = () => {
   const navigate = useNavigate();
-  const { setAlertError } = useSetAlert();
+  const [, setSearchResult] = useRecoilState(searchResultState);
+  const { setAlertError, setAlertSuccess } = useSetAlert();
   const { register, reset, handleSubmit } = useForm<SearchValue>({
     mode: 'onChange',
     defaultValues: {
@@ -16,11 +19,13 @@ const useSearchForm = () => {
   const handleRecipeSearch = handleSubmit(async (data) => {
     const { keyword } = data;
     const res = await searchKeywordRequest(keyword);
-    console.log(res);
     if (!res.success || res.result.length === 0) {
       setAlertError({ error: '검색 결과가 존재하지 않습니다.' });
     } else {
-      navigate(`/search/${keyword}`);
+      const { result, message } = res;
+      setAlertSuccess({ success: message });
+      setSearchResult(result);
+      navigate({ pathname: '/search', search: `keyword=${keyword}` });
     }
   });
 
