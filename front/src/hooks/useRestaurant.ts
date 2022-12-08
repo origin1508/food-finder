30cdf76445d8102493402d5e6d65fbfb31490f04
origin1508 <origin1508@gmail.com>
2022-12-08 16:02:01 +0900

    fix: setLikedRestaurantId 추가

--- front/src/hooks/useRestaurant.ts
+++ front/src/hooks/useRestaurant.ts
@@ -1,4 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { useRecoilState } from 'recoil';
 import { AxiosError } from 'axios';
 import useSetAlert from './useSetAlert';
 import {
@@ -6,19 +7,22 @@ import {
   restaurantLikeRequest,
   restaurantUnlikeRequest,
 } from '../api/restaurantFetcher';
+import { likedRestaurantIdState } from '../atom/restaurant';
 import { LikedRestaurantQuery } from '../types/restaurant/restaurantType';
 
 const useRestaurant = () => {
   const queryClient = useQueryClient();
+  const [, setLikedRestaurantId] = useRecoilState(likedRestaurantIdState);
   const { setAlertError, setAlertSuccess } = useSetAlert();
 
   const restaurantLike = async (
     result: kakao.maps.services.PlacesSearchResultItem,
   ) => {
-    const { place_name, place_url, road_address_name, address_name, x, y } =
+    const { id, place_name, place_url, road_address_name, address_name, x, y } =
       result;
     const res = await restaurantLikeRequest({
       title: place_name,
+      restaurantId: Number(id),
       address: address_name,
       roadAddress: road_address_name,
       mapX: Number(x),
@@ -29,8 +33,8 @@ const useRestaurant = () => {
     return res;
   };
 
-  const restaurantUnlike = async (place_name: string) => {
-    const res = await restaurantUnlikeRequest(place_name);
+  const restaurantUnlike = async (restaurantId: number) => {
+    const res = await restaurantUnlikeRequest(restaurantId);
 
     return res;
   };
@@ -38,6 +42,17 @@ const useRestaurant = () => {
   const likedRestaurantQuery = useQuery<LikedRestaurantQuery>(
     'likedRestaurant',
     getLikedRestaurant,
+    {
+      onSuccess: (data) => {
+        if (data) {
+          const { result } = data;
+          const likedRestaurantId = result.reduce<number[]>((acc, cur) => {
+            return [...acc, cur.restaurant_id];
+          }, []);
+          setLikedRestaurantId(likedRestaurantId);
+        }
+      },
+    },
   );
 
   const restaurantLikeMutation = useMutation(restaurantLike, {
