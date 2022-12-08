8719fa2d3680cbd504f2baf55f969aa2ef486cc3
dlzagu <eodnsdlekd@naver.com>
2022-12-08 23:27:09 +0900

    feat : added comment fetcher

--- front/src/api/recipeFetcher.ts
+++ front/src/api/recipeFetcher.ts
@@ -41,11 +41,61 @@ export async function getRecipeRanking() {
 }
 
 export async function getRecipeDetail(userId: string) {
-  const res = await customAxios.get(`/test/recipes/${userId}`, {
+  const res = await customAxios.get(`/recipes/${userId}`, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${JWT_TOKEN}`,
     },
   });
   return res.data.result;
+}
+
+export async function recipeCommentRequest({
+  recipeId,
+  comment,
+}: {
+  recipeId: string;
+  comment: string;
+}) {
+  const res = await customAxios.post(
+    `/recipes/${recipeId}/comments`,
+    { content: comment },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${JWT_TOKEN}`,
+      },
+    },
+  );
+  return res.data;
+}
+
+export async function recipeCommentUpdate({
+  recipeId,
+  comment,
+}: {
+  recipeId: string;
+  comment: string;
+}) {
+  const res = await customAxios.patch(
+    `/recipes/comments/${recipeId}`,
+    { content: comment },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${JWT_TOKEN}`,
+      },
+    },
+  );
+  return res.data;
+}
+
+export async function recipeCommentDelete({ recipeId }: { recipeId: string }) {
+  const res = await customAxios.delete(`/recipes/comments/${recipeId}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${JWT_TOKEN}`,
+    },
+  });
+  return res.data;
 }
