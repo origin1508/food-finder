2b70cd57fb3eb5aaad8f12a745b2ed4627dd8e0c
origin1508 <origin1508@gmail.com>
2022-12-08 18:23:38 +0900

    fix: api 연동 수정

--- front/src/hooks/useCreateRecipe.ts
+++ front/src/hooks/useCreateRecipe.ts
@@ -1,10 +1,13 @@
+import { AxiosError } from 'axios';
 import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 import useSetAlert from './useSetAlert';
 import imageResize from '../util/imageResize';
 import { createRecipeRequest } from '../api/recipeFetcher';
 import { RecipeFormDefaultValue } from '../types/recipe/recipeFormType';
 
 const useCreateRecipe = () => {
+  const navigate = useNavigate();
   const { setAlertSuccess, setAlertError } = useSetAlert();
   const createRecipe = async (data: RecipeFormDefaultValue) => {
     const {
@@ -17,24 +20,23 @@ const useCreateRecipe = () => {
       ingredients,
       instructions,
     } = data;
-    const ingredient = 'test';
-    const stepImages = Array<Blob>();
+    const stepImages = Array<File>();
     const recipeThumbnail = await imageResize(mainImage.files[0]);
     const steps = await instructions.reduce(async (acc, cur, idx) => {
       const { description, image } = cur;
       const compressedImage = await imageResize(image[0]);
       compressedImage && stepImages.push(compressedImage);
+      stepImages.push(image[0]);
       return { ...acc, [idx + 1]: description };
     }, {});
-
     const formData = new FormData();
     formData.append('name', name);
     formData.append('method', method);
     formData.append('category', category);
-    formData.append('ingredient', ingredient);
+    formData.append('ingredient', JSON.stringify(ingredients));
     formData.append('serving', serving);
     formData.append('cookingTime', cookingTime);
-    formData.append('recipeThumnail', recipeThumbnail);
+    formData.append('recipeThumbnail', recipeThumbnail);
     stepImages.forEach((stepImage) => {
       formData.append('stepImages', stepImage);
     });
@@ -45,11 +47,12 @@ const useCreateRecipe = () => {
 
   const createRecipeMutation = useMutation(createRecipe, {
     onSuccess: (data) => {
-      console.log(data);
-      setAlertSuccess({ success: '성공' });
+      const { message } = data;
+      setAlertSuccess({ success: message });
+      navigate('/collectRecipes');
     },
     onError: (error) => {
-      if (error instanceof Error) {
+      if (error instanceof AxiosError) {
         setAlertError({ error: error.message });
       } else {
         setAlertError({ error: 'request error' });
