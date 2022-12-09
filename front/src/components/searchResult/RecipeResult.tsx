c7d8dac15e02256de5d0bc2551f004e31392f812
origin1508 <origin1508@gmail.com>
2022-12-09 23:55:54 +0900

    fix: 검색결과에 맞춰 사진 변경

--- front/src/components/searchResult/RecipeResult.tsx
+++ front/src/components/searchResult/RecipeResult.tsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
+import { useNavigate } from 'react-router';
 import styled from 'styled-components';
 import RecipeCard from '../recipe/RecipeCard';
 import CustomIcon from '../icons/CustomIcon';
@@ -8,11 +10,24 @@ import { BigTitle, MediumTitle } from '../../styles/commonStyle';
 
 const RecipeResult = ({ keyword }: SearchValue) => {
   const searchResult = useRecoilValue(searchResultState);
+  const images = searchResult.reduce<string[]>((acc, cur) => {
+    return [...acc, cur.image_url1];
+  }, []);
+  const [imageIndex, setImageIndex] = useState(0);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const loop = setInterval(() => {
+      setImageIndex((prev) => (prev + 1 >= images.length ? 0 : prev + 1));
+    }, 5000);
+    return () => {
+      clearInterval(loop);
+    };
+  }, [searchResult, imageIndex, setImageIndex]);
+
   return (
     <RecipeResultContainer>
-      <RecipeResultImg>
-        <RecipeResultImgTitle>{keyword}</RecipeResultImgTitle>
-      </RecipeResultImg>
+      <RecipeResultImg url={images[imageIndex]} />
+      <RecipeResultImgTitle>{keyword}</RecipeResultImgTitle>
       <RecipeResultTitle>
         <CustomIcon name="quoteLeft" size="16" />
         {keyword}
@@ -31,13 +46,16 @@ const RecipeResult = ({ keyword }: SearchValue) => {
           } = item;
           return (
             <RecipeCard
+              key={dish_id}
               img={image_url1}
               title={name}
               channelUuid={dish_id}
               views={views}
               likes={likes}
               creator={nickname}
-              onMoreClick={() => {}}
+              onClickDetailPage={() => {
+                navigate(`/recipe/detail/${dish_id}`);
+              }}
             />
           );
         })}
@@ -50,28 +68,32 @@ export default RecipeResult;
 
 const RecipeResultContainer = styled.section`
   ${({ theme }) => theme.mixins.flexBox('column')};
+  position: relative;
   width: 100%;
   height: 80%;
-  padding: 0 ${({ theme }) => theme.spacingLarge};
   gap: ${({ theme }) => theme.spacingLarge};
 `;
 
-const RecipeResultImg = styled.div`
+const RecipeResultImg = styled.div<{ url: string }>`
   height: 40vh;
   width: 100vw;
   text-align: center;
-  background-image: url(https://images.pexels.com/photos/13774731/pexels-photo-13774731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
+  ${({ url }) => `background-image: url('${url}')`};
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
   background-color: ${({ theme }) => theme.mainBlack};
+  transition: all 1.5s;
 `;
 
 const RecipeResultImgTitle = styled.h2`
   ${BigTitle};
-  width: 100%;
-  height: 100%;
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  height: 40vh;
   line-height: 40vh;
+  text-align: center;
   background-color: rgba(0, 0, 0, 0.4);
 `;
 
@@ -82,7 +104,17 @@ const RecipeResultTitle = styled.h2`
 `;
 
 const RecipeResultList = styled.div`
-  ${({ theme }) => theme.mixins.flexBox()}
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  place-items: center;
   width: 100%;
+  height: 26vh;
   gap: ${({ theme }) => theme.spacingLarge};
+`;
+
+const Temp = styled.div<{ url: string }>`
+  ${({ url }) => `background-image: url('${url}')`};
+  width: 10rem;
+  height: 10rem;
+  background-color: black;
 `;
