239bcc237e08b7415c52f36c7b37d8867dee681d
origin1508 <origin1508@gmail.com>
2022-12-12 16:41:22 +0900

    fix: recipe data 연동

--- front/src/pages/editRecipe.tsx
+++ front/src/pages/editRecipe.tsx
@@ -1,26 +1,49 @@
 import styled from 'styled-components';
 import { FormProvider, useForm } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
 import useCreateRecipe from '../hooks/Recipe/useCreateRecipe';
+import { useRecipeDetail } from '../hooks/Recipe/useRecipes';
 import BasePageComponent from '../hoc/BasePageComponent';
 import RecipeForm from '../components/recipeForm/RecipeForm';
 import { RecipeFormDefaultValue } from '../types/recipe/recipeFormType';
 import Auth from '../hoc/Auth';
 
 const EditRecipe = () => {
+  const { recipeId } = useParams();
+  const { data: recipeDetail } = useRecipeDetail(recipeId!);
+  const {
+    name,
+    serving,
+    cookingTime,
+    category,
+    method,
+    largeThumbnailUrl,
+    ingredient,
+    Steps,
+  } = recipeDetail!;
+  const instructions = Steps.reduce<
+    { description?: string; preview?: string }[]
+  >((acc, cur) => {
+    return [
+      ...acc,
+      {
+        description: cur.content,
+        preview: cur.imageUrl,
+      },
+    ];
+  }, []);
+
   const methods = useForm<RecipeFormDefaultValue>({
     mode: 'onChange',
     defaultValues: {
-      name: '소고기 미역국',
-      serving: '1인분',
-      cookingTime: '30분',
-      category: '국&찌개',
-      method: '끓이기',
-      ingredients: [
-        { name: '돼지고기', amount: '300g' },
-        { name: '양배추', amount: '1/2개' },
-        { name: '참기름', amount: '2t' },
-      ],
-      instructions: [{ description: '' }],
+      name: name,
+      mainImage: { preview: largeThumbnailUrl },
+      serving: String(serving),
+      cookingTime: String(cookingTime),
+      category: category,
+      method: method,
+      ingredients: JSON.parse(ingredient),
+      instructions: instructions,
     },
   });
   const { formState } = methods;
